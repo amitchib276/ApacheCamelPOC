0502a377b0ce58d8694929f61c6c7ff8cdcc052a
BM00344124 <BM00344124@INBASDPC05648.TechMahindra.com>
2019-08-05 18:38:13 +0530

    "Files committed for filteration"

--- ApacheCamel/src/main/java/org/mycompany/RouteHandling/ContentBasedRouting.java
+++ ApacheCamel/src/main/java/org/mycompany/RouteHandling/ContentBasedRouting.java
@@ -11,7 +11,7 @@ public class ContentBasedRouting extends DefaultCamelContext{
 		CamelContext context = new DefaultCamelContext();
 		 context.addRoutes(new RouteBuilder() {
 
-			@Override
+			/*@Override
 			public void configure() throws Exception {
 				
 				from("file:target/cbr/input?noop=true&fileName=Order.xml").log("Split by Order  Element").split(xpath("Orders/Order"))
@@ -19,7 +19,19 @@ public class ContentBasedRouting extends DefaultCamelContext{
 				.when(xpath("Order/Country='USA'")).to("file:target/cbr/output/US")				
 				.when(xpath("Order/Country='UK'")).to("file:target/cbr/output/UK");
 				
+			}*/
+			 
+			 
+			 // Header based routing
+			 @Override
+			public void configure() throws Exception {
+
+				from("file:target/cbr/input?noop=true&fileName=Order.xml").log("Split by Order  Element").choice()
+						.when(header("foo").isEqualTo("test")).to("file:target/cbr/output/HeaderFoo").otherwise().to("seda:xyz");
+
 			}
+			 
+			 
 			 
 		 });
 		 
